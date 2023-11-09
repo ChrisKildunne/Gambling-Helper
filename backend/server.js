ce3cdaa02caa3d3c89ee7a63b7aa224f2e821d05
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-11-09 16:12:21 -0700

    accessing odds api succesfull

--- backend/server.js
+++ backend/server.js
@@ -8,20 +8,29 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 
 app.get('/api/odds', async (req, res) => {
+    const sport = 'americanfootball_nfl'; 
+    const apiKey = 'ec9e28f0879758ec93d0b2bb04080e36'; 
+    const regions = 'us';
+    const markets = 'h2h,spreads';
+
+    const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds/`;
+    const params = {
+        apiKey: apiKey,
+        regions: regions,
+        markets: markets,
+        oddsFormat: 'american'
+    };
+
     try {
-        const response = await axios.get('https://api.the-odds-api.com/v4/sports/?apiKey=process.env.ODDS_API_KEY', {
-            params: {
-                apiKey: process.env.ODDS_API_KEY,
-                sport: 'soccer_epl', // Example: English Premier League soccer
-                region: 'uk', // Example region
-                mkt: 'h2h', // Example market type
-            }
-        });
+        const response = await axios.get(url, { params });
+        console.log('API Response:', response.data);
         res.json(response.data);
     } catch (error) {
+        console.error('Error:', error);
         res.status(500).json({ message: error.message });
     }
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
